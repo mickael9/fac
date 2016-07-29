d9e3dc12976e07c3bf4fa778682cbfefc910786e
Mickaël Thomas <mickael9@gmail.com>
2016-07-29 19:49:01 +0200

    Release 0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 setup(
     name='fac-cli',
 
-    version='0.2',
+    version='0.3',
 
     description='Command-line mod manager for Factorio (install, update...).',
     long_description=long_description,

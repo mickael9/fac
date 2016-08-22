9b83c2ad81c555e7989e773f027cbad6a941c73e
Mickaël Thomas <mickael9@gmail.com>
2016-08-22 19:11:06 +0200

    Show mods version in list command

--- fac/commands/list.py
+++ fac/commands/list.py
@@ -11,12 +11,12 @@ class ListCommand(Command):
         mods = self.manager.get_installed_mods()
         for mod in mods:
             n = int(not self.manager.is_mod_enabled(mod.name))
-            categories[n].append(mod.name)
+            categories[n].append(mod)
 
         for i, name in enumerate(('Enabled', 'Disabled')):
             mods = categories[i]
             if mods:
                 print('%s mods:' % name)
                 for mod in mods:
-                    print('    %s' % mod)
+                    print('    %s %s' % (mod.name, mod.version))
                 print()

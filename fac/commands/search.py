08c4903d5a03e755945db7178b69d1687748593a
Mickaël Thomas <mickael9@gmail.com>
2016-08-23 23:49:54 +0200

    Add -l, --limit to `search` command

--- fac/commands/search.py
+++ fac/commands/search.py
@@ -26,13 +26,17 @@ class SearchCommand(Command):
             action='store_const',
             dest='sort',
             const='updated'),
+
+        Arg('-l', '--limit', type=int,
+            help='only show that many results'),
     ]
 
     def run(self, args):
         for result in self.api.search(
                 query=args.query or '',
                 tags=tuple(args.tag),
-                order=args.sort):
+                order=args.sort,
+                limit=args.limit):
 
             if result.tags:
                 tags = ' [%s]' % (', '.join(tag.name for tag in result.tags))

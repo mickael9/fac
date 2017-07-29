7c0e381ae3033777b3c1b4354e98bfe7c49f5d00
Mickaël Thomas <mickael9@gmail.com>
2017-07-29 18:13:49 +0200

    Add parser support for subcommands

--- fac/commands/__init__.py
+++ fac/commands/__init__.py
@@ -7,12 +7,17 @@ class CommandRegistry(type):
 
     def __init__(cls, name, bases, dict):
         super().__init__(name, bases, dict)
-        if 'name' in dict:
+        cls.subcommands = []
+
+        if 'parent' in dict:
+            dict['parent'].subcommands.append(cls)
+        elif 'name' in dict:
             CommandRegistry.commands.append(cls)
 
 
 class Command(metaclass=CommandRegistry):
     arguments = []
+    usage = None
     help = ''
     epilog = ''
 
@@ -27,19 +32,36 @@ class Command(metaclass=CommandRegistry):
         epilog = textwrap.dedent(self.epilog.strip('\n'))
         help = self.help or (description and description.splitlines()[0])
 
-        parser = subparser.add_parser(
+        command_parser = argparse.ArgumentParser(add_help=False)
+        group = command_parser.add_argument_group('%s options' % self.name)
+        for args, kwargs in self.arguments:
+            group.add_argument(*args, **kwargs)
+
+        subcommands_parser = argparse.ArgumentParser(add_help=False, usage='%(prog)s ' + self.name)
+        if self.subcommands:
+            subcommand_parser = subcommands_parser.add_subparsers(
+                metavar='SUBCOMMAND',
+                title=None,
+            )
+            for subcommand_class in self.subcommands:
+                subcommand = subcommand_class(self.api, self.config, self.manager)
+                subcommand.create_parser(subcommand_parser, [command_parser] + parents)
+
+        self.parser = subparser.add_parser(
             self.name,
+            usage=self.usage,
             help=help,
             description=description,
             epilog=epilog,
-            parents=parents,
+            parents=[subcommands_parser, command_parser] + parents,
             add_help=False,
             formatter_class=argparse.RawDescriptionHelpFormatter,
         )
-        for args, kwargs in self.arguments:
-            parser.add_argument(*args, **kwargs)
-        parser.set_defaults(run=self.run)
 
+        self.parser.set_defaults(run=self.run)
+
+    def run(self, args):
+        self.parser.print_help()
 
 def Arg(*args, **kwargs):
     return args, kwargs
